322a2212a4e5ff49c5dacf8ff9fb58573d24b883
RasSilasMotse <ras@rassilasmotse.co.za>
2025-06-26 09:42:46 +0100

    This is my practical practicing Python

---
+++ operators.py
@@ -0,0 +1,24 @@
+#operators
+
+# Addition (+)
+# Subtraction (-)
+# Multiplication (*)
+# Division (/)
+# Modulus (%)
+# Exponent (**)
+
+x = 10 
+y = 2
+
+print (x+y)
+print (x-y)
+print (x*y)
+print (x/y)
+print (x%y)
+print (x**y)
+print (5%2)
+
+x = 10
+
+x -= 2
+print(x)
